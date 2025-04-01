c534f083557d73a48db9f2de19b6f6b344e40bf8
WiLait <wilait.xaxa@gmail.com>
2025-04-02 00:12:35 +0600

    Puchenko 2.0

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("word")
+#print("word")
+
+from getpass import getpass
+from _mysql_connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Имя пользователя: "),
+        password=getpass("Пароль: "),
+    ) as connection:
+        printf(connection)
+except Error as e:
+    printf(e)
