8954c8c275ff4f550b9b0b4c37a31559b3784ad1
WiLait <wilait.xaxa@gmail.com>
2025-04-07 18:20:40 +0600

    Puchenko 2.0. Создал файл login и связал его с Pycharm

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ data = [
     ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
     ("Monty Python's The Meaning of Life", 1983, 7.5),
     ("Monty Python's Life of Brian", 1979, 8.0),
+    ("Monty Puthon Kis Kis", 2003, 9.9),
 ]
 
 cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
--- create.py
+++ create.py
@@ -1,81 +1,16 @@
-import mysql.connector
-from config import DB_CONFIG
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import uic
+import os  # Заменяем pathlib
 
+# Укажите путь к вашему файлу UI
+ui_path = "C:/Users/User/Documents/PyQt6/login_form.ui"
 
-def create_empty_database():
-    """Создает пустую базу данных (если не существует)"""
-    try:
-        # Подключаемся без указания базы данных
-        conn = mysql.connector.connect(
-            host=DB_CONFIG['host'],
-            user=DB_CONFIG['user'],
-            password=DB_CONFIG['password']
-        )
-        cursor = conn.cursor()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
-        # Создаем базу данных (если её нет)
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_CONFIG['database']}")
-        print(f"База данных '{DB_CONFIG['database']}' готова к использованию")
+    # Загружаем UI напрямую, без класса
+    window = uic.loadUi(ui_path)
+    window.show()
 
-        cursor.close()
-        conn.close()
-    except Exception as e:
-        print(f"Ошибка: {e}")
-
-
-if __name__ == "__main__":
-    create_empty_database()
-
-
-
-
-
-
-
-
-
-
-
-
-
-#"""
-#import mysql.connector
-#import PySide6
-#from config import DB_CONFIG
-#
-#def create_table():
-
-#    conn = mysql.connector.connect(**DB_CONFIG)
-#    cursor = conn.cursor()
-    
-#    cursor.execute("""
-#    CREATE TABLE IF NOT EXISTS sample_data (
-#        id INT AUTO_INCREMENT PRIMARY KEY,
-#        item_number INT NOT NULL,
-#        product_name VARCHAR(100) NOT NULL,
-#        price DECIMAL(10, 2) NOT NULL
-#    )
-#    """)
-    
-    # Добавляем тестовые данные
-#    sample_data = [
- #       (1, 'Ноутбук', 899.99),
-  #      (2, 'Смартфон', 599.50),
-   #     (3, 'Наушники', 149.99)
-    #]
-    
- #   cursor.executemany("""
- #   INSERT INTO sample_data (item_number, product_name, price)
-  ##  VALUES (%s, %s, %s)
-   ## ON DUPLICATE KEY UPDATE
-#    product_name = VALUES(product_name),
- #   price = VALUES(price)
-  #  """, sample_data)
-    
-#    conn.commit()
-#    cursor.close()
-#    conn.close()
-#    print("Таблица создана и заполнена тестовыми данными")
-
-#if __name__ == "__main__":
-#    create_table() """
+    sys.exit(app.exec())
